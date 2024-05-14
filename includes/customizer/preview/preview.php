1bb9d3819d5e405a24a46aa5d6b7c61629ce3065
Vedant Dighe <91383099+vedantdighe@users.noreply.github.com>
2024-05-15 03:57:55 +0530

    Update preview.php

--- includes/customizer/preview/preview.php
+++ includes/customizer/preview/preview.php
@@ -8,12 +8,12 @@ if (!defined('ABSPATH')) {
 	<head>
 		<meta charset="<?php bloginfo('charset'); ?>" />
 		<meta name="viewport" content="width=device-width" />
-			<style type="text/css" id="cev_designer_custom_css">.woocommerce-store-notice.demo_store, .mfp-hide {display: none;}</style>
+			<style type="text/css" id="evfwr_designer_custom_css">.woocommerce-store-notice.demo_store, .mfp-hide {display: none;}</style>
 	</head>
-	<body class="cev_preview_body">
+	<body class="evfwr_preview_body">
 		<div id="overlay"></div>
-		<div id="cev_preview_wrapper" style="display: block;">
-			<?php cev_initialise_customizer_settings::preview_account_email(); ?>
+		<div id="evfwr_preview_wrapper" style="display: block;">
+			<?php evfwr_initialise_customizer_settings::preview_account_email(); ?>
 		</div>
 		<?php
 		do_action( 'woomail_footer' );
