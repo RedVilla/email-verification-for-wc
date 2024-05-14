3e8c5bec2e35cd845f2301609a030a5001d66bfb
Vedant Dighe <91383099+vedantdighe@users.noreply.github.com>
2024-05-15 04:02:35 +0530

    Update verification-widget-style.php

--- includes/customizer/verification-widget-style.php
+++ includes/customizer/verification-widget-style.php
@@ -7,7 +7,7 @@
 /**
  * Adds the individual sections, settings, and controls to the theme customizer
  */
-class Cev_Verification_Widget_Style {
+class evfwr_Verification_Widget_Style {
 	// Get our default values	
 	private static $order_ids  = null;
 	
@@ -22,7 +22,7 @@ class Cev_Verification_Widget_Style {
 	 * @return bool
 	 */
 	public static function is_own_preview_request() {
-		return isset( $_REQUEST['action'] ) && 'preview_cev_verification_lightbox' === $_REQUEST['action'];
+		return isset( $_REQUEST['action'] ) && 'preview_evfwr_verification_lightbox' === $_REQUEST['action'];
 	}
 	
 	/**
@@ -31,7 +31,7 @@ class Cev_Verification_Widget_Style {
 	 * @return bool
 	 */
 	public static function is_own_customizer_request() {
-		return isset( $_REQUEST['section'] ) && 'cev_verification_widget_style' === $_REQUEST['section'];
+		return isset( $_REQUEST['section'] ) && 'evfwr_verification_widget_style' === $_REQUEST['section'];
 	}		
 }
 
@@ -39,4 +39,4 @@ class Cev_Verification_Widget_Style {
  * Initialise our Customizer settings
  */
 
-$cev_verification_widget_style = new Cev_Verification_Widget_Style();
+$evfwr_verification_widget_style = new evfwr_Verification_Widget_Style();
