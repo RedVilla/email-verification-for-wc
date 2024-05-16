236403a5554f196573b35afd69359318cc5e9c6c
Vedant Dighe <91383099+vedantdighe@users.noreply.github.com>
2024-05-17 01:43:50 +0530

    Update class-wc-customer-email-verification-email-common.php

--- includes/class-wc-customer-email-verification-email-common.php
+++ includes/class-wc-customer-email-verification-email-common.php
@@ -286,7 +286,7 @@ class WooCommerce_Customer_Email_Verification_Email_Common {
 		$pin = ''; //our default pin is blank.
 		while ( $i < $digits ) {
 			//generate a random number between 0 and 9.
-			$pin .= mt_rand(0, 9);
+			$pin .= wp_rand(0, 9);
 			$i++;
 		}
 		return $pin;
