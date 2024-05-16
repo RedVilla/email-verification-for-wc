9cbe6e625d2378c97049704ef1944293fab74509
Vedant Dighe <91383099+vedantdighe@users.noreply.github.com>
2024-05-17 01:36:12 +0530

    Update custom-controls.php

--- includes/customizer/custom-controls.php
+++ includes/customizer/custom-controls.php
@@ -10,7 +10,7 @@ if ( class_exists( 'WP_Customize_Control' ) ) {
 		public function render_content() {
 			?>
 			<label>
-				<h3 class="customize-control-title"><?php esc_html_e( $this->label, 'customer-email-verification-for-woocommerce' ); ?></h3>
+				<h3 class="customize-control-title"><?php esc_html( $this->label, 'customer-email-verification-for-woocommerce' ); ?></h3>
 				<?php if ( ! empty( $this->description ) ) : ?>
 				<span class="description customize-control-description"><?php echo wp_kses_post( $this->description ); ?></span>
 				<?php endif; ?>
